5ce4383049a8112887036df5932c33cc76cc55ec
Byron <80455013+jyronbones@users.noreply.github.com>
2021-12-31 04:42:14 -0500

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,4 +1,4 @@
-import javax.security.auth.login.LoginException; 
+import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
