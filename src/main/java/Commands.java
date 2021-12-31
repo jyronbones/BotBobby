5f7a0c9e9031e2c7e907eeda6a2dca96a30cefc8
Byron <80455013+jyronbones@users.noreply.github.com>
2021-12-31 04:26:10 -0500

    Update Commands.java

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,50 +1,56 @@
 import java.io.IOException;
-
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+/**
+ * This class contains the methods with commands to listen for user input discord
+ */
 public class Commands extends ListenerAdapter {
-	
+
+	/**
+	 * This method executes scraping of sites upon received command and provided embedded end of search message
+	 * @param event is the passed event of a user command
+	 */
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		String[] args = event.getMessage().getContentRaw().split("\\s+");
 		
-		if(args[0].equalsIgnoreCase(Bot.prefix + "gpu")) {
-			EmbedBuilder info = new EmbedBuilder();
-			info.setTitle("(☞ﾟヮﾟ)☞🕹30x Graphics Cards in Store🕹☜(ﾟヮﾟ☜)");
-			info.setDescription("⬇Graphics card inventories⬇");
-            
+		if(args[0].equalsIgnoreCase(Bot.PREFIX + "gpu")) {
+
 			try {
 			NewEgg ne = new NewEgg();
-				ne.newEgg();
+			ne.newEggScrape(event);
 
             CanadaComputers cc = new CanadaComputers();
-            cc.canadaComputers();
+            cc.canadaComputersScrape(event);
 
             MemoryExpress me = new MemoryExpress();
-            me.memoryExpress();
-            
-            Crypto crypt = new Crypto();
-            crypt.cryptoCurrency();
-            
+            me.memoryExpressScrape(event);
+
+            BestBuy bb = new BestBuy();
+            bb.bestBuyScrape(event);
+
             } catch (IOException e) {
-				// TODO Auto-generated catch block
+				System.err.println("Error running scrape methods");
 				e.printStackTrace();
 			}
-			
-			if(args[0].equalsIgnoreCase(Bot.prefix + "shiba")) {
+
+			if(args[0].equalsIgnoreCase(Bot.PREFIX + "shiba")) {
 				EmbedBuilder info2 = new EmbedBuilder();
 				info2.setTitle("Cryptocurrency values");
 			}
 
+			EmbedBuilder info = new EmbedBuilder();
+			info.setTitle("(☞ﾟヮﾟ)☞🕹30x Graphics Cards in Store🕹☜(ﾟヮﾟ☜)");
+			info.setImage("https://cdn.mos.cms.futurecdn.net/4SQBx6PXfEMhitPwz32eFM-970-80.jpg.webp");
+			info.setDescription("⬇Graphics card inventories⬇");
 			info.addField("End of stock", "Check back later", false);
 			info.setColor(0xff1f1f);
 			info.setFooter("＼(ﾟｰﾟ＼)", event.getMember().getUser().getAvatarUrl());
-			
+
 			event.getChannel().sendTyping().queue();
 			event.getChannel().sendMessage(info.build()).queue();
 			info.clear();
 		}
 	}
-
 }
