0294e97c378e7d9d0dca362c4b6c78d4e93ac115
Byron <80455013+jyronbones@users.noreply.github.com>
2021-12-31 04:30:35 -0500

    Update MemoryExpress.java

--- src/main/java/MemoryExpress.java
+++ src/main/java/MemoryExpress.java
@@ -1,19 +1,25 @@
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
-
+/**
+ * This class contains the methods to scrape 30x series GPUs from Memory Express website and relay to discord
+ */
 public class MemoryExpress {
-
-    public void memoryExpress() throws IOException {
+    /**
+     * This method scrapes Memory Express 30x series graphics cards from website and displays via an embedded text
+     * @param event is the passed event as a command
+     * @throws IOException is the exception class that is thrown if IO error
+     */
+    public void memoryExpressScrape(GuildMessageReceivedEvent event) throws IOException {
 
         int pageCount = 1;
-        while(pageCount <= 1) //38 pages
+        while(pageCount <= 3)
         {
-            //30x series url: https://www.memoryexpress.com/Category/VideoCards?FilterID=06feb6a8-fb17-71c3-bd4b-3f3b39562041
-            String meUrl = "https://www.memoryexpress.com/Category/VideoCards?FilterID=c03cd432-a0a4-1828-9edb-830342fb1ad4&Page=" + pageCount;
+            String meUrl = "https://www.memoryexpress.com/Category/VideoCards?FilterID=a3bbaaca-8a1f-2941-1da3-fb220ebcaf1b&Page=" + pageCount;
 
             pageCount++;
 
@@ -21,7 +27,7 @@ public class MemoryExpress {
 
             Elements body = doc.select(".c-shca-container");
 
-            for(Element t: body.select(".c-shca-icon-item"))
+            for(Element t: body.select(".c-shca-icon-item")) // loops through the element which holds the products on the page
             {
                 Elements title = t.select(".c-shca-icon-item__body-name");
                 String image = t.select(".c-shca-icon-item__body-image img").attr("src");
@@ -29,17 +35,22 @@ public class MemoryExpress {
                 String link = t.select("a").attr("href");
                 Elements inStock = t.select(".c-shca-icon-item__body-inventory");
 
-                if(!inStock.text().equals("Out of Stock"))
+                if(!inStock.text().equals("Out of Stock")) // if item available for purchase
                 {
-                    System.out.println("\n" + title.text());
-                    System.out.println(image);
-                    System.out.println(price.text().replace("+", ""));
-                    System.out.println("https://www.memoryexpress.com" + link);
-                    System.out.println("In Stock");
+                        EmbedBuilder info = new EmbedBuilder();
+                        info.setTitle(title.text());
+                        info.setDescription("⬇Memory Express⬇");
+                        info.setImage(image);
+                        info.addField("Price", price.text().replace("+", ""), false);
+                        info.addField("Website","https://www.memoryexpress.com" + link, false);
+                        info.addField("Availability","In Stock", false);
+                        info.setThumbnail("https://d2t1xqejof9utc.cloudfront.net/screenshots/pics/2a34a345be3bf4b30229de096bd0b06e/large.jpg");
+
+                        event.getChannel().sendTyping().queue();
+                        event.getChannel().sendMessage(info.build()).queue();
 
                 }
             }
         }
-
     }
 }
